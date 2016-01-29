8750f26ab4e10444201a3d99657a48b749196847
Julien Nauroy <julien.nauroy@gmail.com>
2016-01-29 22:08:43 +0100

    updated the setup scrit

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ from setuptools import find_packages
 
 setup(name='python-itop-api',
       version='1.0',
-      description='Set of python script to interact with iTop',
-      author='Guillaume Philippon',
-      author_email='guillaume.philippon@lal.in2p3.fr',
+      description='Set of python scripts to interact with iTop',
+      author='Guillaume Philippon, Julien Nauroy',
+      author_email='guillaume.philippon@lal.in2p3.fr, julien.nauroy@u-psud.fr',
       url='https://github.com/guillaume-philippon/python-itop-api',
       license='FreeBSD License',
-      data_files=[('/usr/share/itop-cli', ['itop-cli.cfg.example'])],
-      scripts=["itop-cli"],
+      data_files=[('/usr/share/itop-cli', ['python-itop-api.cfg.example'])],
+      scripts=["itop-cli", "vcenter2itop", "itop2centreon"],
       packages=find_packages())
